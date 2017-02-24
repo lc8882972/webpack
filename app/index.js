b6505d74efdf012844a9be83dde8ab9ae9380f11
张国延 <410115424@qq.com>
2017-02-24 17:16:39 +0800

    use service worker

--- app/index.js
+++ app/index.js
@@ -7,3 +7,11 @@ console.log($('#say').text());
 var point = new Point(100, 100);
 
 console.log( utils.isNumber(1));
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+        console.log('service worker 注册成功');
+    }).catch(function (err) {
+        console.log('servcie worker 注册失败')
+    });
+}
---
+++ service-worker.js
@@ -0,0 +1,49 @@
+var cacheFiles = [
+    'index.html',
+    'styles.css',
+    '/assets/jquery.min.js',
+    'bundle.js',
+    '/assets/f9198618367adab48fce67c78bd4b31c8701e41c.jpg'
+];
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open('v1').then(function (cache) {
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    // console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+
+        caches.open('v1').then(function (cache) {
+
+            return cache.match(event.request)
+                .then(function (response) {
+                    if (response) {
+                        return response;
+                    }
+                    console.log(event.request);
+                    fetch(event.request).then(function (response) {
+                        console.log('found in network');
+                        return response;
+                    }).catch(function (error) {
+                        console.log(error);
+                        throw error;
+                    });
+
+                }).catch(function (error) {
+                    console.log(error);
+                    return error;
+                });
+        })
+    );
+});
+
+self.addEventListener('sync', function (event) {
+    // console.log('Handling fetch event for', event.request.url);
+
+    console.log(event);
+});
