88bdb52f2c4fdc46e6e228bb4a994b2eca7d31c5
lc8882972 <guoyan2972@qq.com>
2017-03-22 23:02:46 +0800

    webpack + browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,38 @@
-var JSON5 = require('json5');
-var gulp = require('gulp');
+var gulp = require('gulp')
 
-gulp.task('default',function(){
-    var obj = JSON5.parse('{ "presets": ["es2015"] }');
-    console.log(obj);
-})
+// gulp.task('default', function() {
+//   var obj = JSON5.parse('{ "presets": ["es2015"] }');
+//   console.log(obj);
+// })
+
+
+var browserSync = require('browser-sync')
+var webpack = require('webpack')
+var browserSyncTask = function() {
+  var webpackConfig = require('webpack.config')
+  var compiler = webpack(webpackConfig)
+
+  var server = {
+    "baseDir": "dist"
+  }
+
+  server.middleware = [
+    require('webpack-dev-middleware')(compiler, {
+      stats: 'errors-only',
+      noInfo: true,
+      publicPath: webpackConfig.output.publicPath
+    }),
+    require('webpack-hot-middleware')(compiler, {
+      //   publicPath: 'dist',
+      log: console.log,
+      path: '/__webpack_hmr',
+      heartbeat: 10 * 1000
+    })
+  ]
+
+  browserSync.init(config.tasks.browserSync)
+}
+
+
+
+gulp.task('browserSync', browserSyncTask)
--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,51 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './app/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            // {
-            //     test: /\.js$/,
-            //     use: 'babel-loader'
-            // }, 
+  entry: [
+    'app/index.js',
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+  ],
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: '#source-map',
+  module: {
+    rules: [{
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+              loader: "css-loader" // translates CSS into CommonJS
+            },
             {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
-                            loader: "css-loader" // translates CSS into CommonJS
-                        },
-                        {
-                            loader: "postcss-loader"
-                        },
-                        {
-                            loader: "sass-loader" // compiles Sass to CSS
-                        }
-                    ]
-                })
+              loader: "postcss-loader"
+            },
+            {
+              loader: "sass-loader" // compiles Sass to CSS
             }
-        ]
-    },
-    resolve: {
-        alias: {
-            jquery: "https://code.jquery.com/jquery-3.1.1.slim.min.js"
-        }
-    },
-    plugins: [
-        new ExtractTextPlugin('styles.css'),
-        // new webpack.optimize.UglifyJsPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'index.html'
+          ]
         })
+      }
     ]
+  },
+  resolve: {
+    alias: {
+      jquery: "https://code.jquery.com/jquery-3.1.1.slim.min.js"
+    }
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    // new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new  webpack.NoErrorsPlugin()
+  ]
 }
